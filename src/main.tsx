c05cac1172a945dfdd14a317c76ca0a1eca2700b
boraogunc <126953524+boraogunc@users.noreply.github.com>
2025-11-14 14:40:34 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+import App from "../App";
+import { GameProvider } from "../contexts/GameContext";
+
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <GameProvider>
+      <App />
+    </GameProvider>
   </React.StrictMode>
 );
