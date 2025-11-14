24b58729566018b99fc4b7d03a22d434eabca3e0
boraogunc <126953524+boraogunc@users.noreply.github.com>
2025-11-14 14:41:23 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-
-import React, { useContext } from 'react';
-import { GameContext } from './contexts/GameContext';
-import SplashScreen from './components/SplashScreen';
-import SetupScreen from './components/SetupScreen';
-import GameScreen from './components/GameScreen';
-import JudgingScreen from './components/JudgingScreen';
-import LeaderboardScreen from './components/LeaderboardScreen';
-import { GameScreen as Screen } from './types';
+// App.tsx
+import React, { useContext } from "react";
+import { GameContext } from "./contexts/GameContext";
+import SplashScreen from "./components/SplashScreen";
+import SetupScreen from "./components/SetupScreen";
+import GameScreen from "./components/GameScreen";
+import JudgingScreen from "./components/JudgingScreen";
+import LeaderboardScreen from "./components/LeaderboardScreen";
+import { GameScreen as Screen } from "./types";
 
 const App: React.FC = () => {
   const { screen } = useContext(GameContext);
@@ -24,17 +24,11 @@ const App: React.FC = () => {
       case Screen.LEADERBOARD:
         return <LeaderboardScreen />;
       default:
-        return <SplashScreen />;
+        return <SetupScreen />;
     }
   };
 
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-[#12121c] to-[#2a1a3d] flex flex-col items-center justify-center p-4">
-      <div className="relative w-full max-w-md mx-auto h-[80vh] min-h-[600px] md:h-auto md:min-h-0">
-         {renderScreen()}
-      </div>
-    </div>
-  );
+  return <div className="gradient screen">{renderScreen()}</div>;
 };
 
 export default App;
